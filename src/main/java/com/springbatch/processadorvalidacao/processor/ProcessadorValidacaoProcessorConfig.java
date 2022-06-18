3906c4f8e1cc1509526801414608da53de4da8ab
henrique.franco <henrique.franco.terc@preventsenior.com.br>
2022-06-18 16:54:18 -0300

    criando validadores compostos

--- src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
+++ src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -2,6 +2,9 @@ package com.springbatch.processadorvalidacao.processor;
 
 import com.springbatch.processadorvalidacao.dominio.Cliente;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.CompositeItemProcessor;
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
+import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidationException;
 import org.springframework.batch.item.validator.Validator;
@@ -17,21 +20,28 @@ public class ProcessadorValidacaoProcessorConfig {
     private Set<String> emais = new HashSet<>();
 
     @Bean
-    public ItemProcessor<Cliente, Cliente> processadorValidacaoProcessor(){
-        /***
-         * Validação de propriedades obrigatorias
-         */
-//        BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
-//        processor.setFilter(true);
+    public ItemProcessor<Cliente, Cliente> processadorValidacaoProcessor() throws Exception {
+        return new CompositeItemProcessorBuilder<Cliente, Cliente>()
+                .delegates(beanValidatingItemProcessor(), emailValidatingItemProcessor())
+                .build();
+    }
 
+    private BeanValidatingItemProcessor<Cliente> beanValidatingItemProcessor() throws Exception {
+        BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
+        processor.setFilter(true);
+        processor.afterPropertiesSet();
+        return processor;
+    }
+
+    private ValidatingItemProcessor<Cliente> emailValidatingItemProcessor(){
         ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
         processor.setValidator(validator());
-
         return processor;
     }
 
     /**
      * Customizando validação
+     *
      * @return
      */
     private Validator<Cliente> validator() {
