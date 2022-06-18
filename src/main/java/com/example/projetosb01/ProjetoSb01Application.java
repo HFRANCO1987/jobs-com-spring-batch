349a6105832a220f50e101abd01e3a11ed51d3ba
henrique.franco <henrique.franco.terc@preventsenior.com.br>
2022-06-17 23:47:28 -0300

    commit inicial

---
+++ src/main/java/com/example/projetosb01/ProjetoSb01Application.java
@@ -0,0 +1,13 @@
+package com.example.projetosb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSb01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSb01Application.class, args);
+    }
+
+}
