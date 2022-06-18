19be6e13cf226a2fd084af911e341713b69aab18
henrique.franco <henrique.franco.terc@preventsenior.com.br>
2022-06-18 00:06:39 -0300

    criando chunck simples para para ou impar

--- src/main/java/com/example/projetosb01/ParImparBatchConfig.java
+++ src/main/java/com/example/projetosb01/ParImparBatchConfig.java
@@ -11,19 +11,27 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableBatchProcessing
-public class BatchConfig {
+public class ParImparBatchConfig {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    public BatchConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+    public ParImparBatchConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
     }
@@ -31,17 +39,37 @@ public class BatchConfig {
     @Bean
     public Job imprimeAloJob(){
         return jobBuilderFactory
-                .get("imprimeOlaJob")
-                .start(imprimeOlaStep())
+                .get("imprimeParImparJob")
+                .start(imprimeParImparStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
 
-    public Step imprimeOlaStep(){
+    /**
+     * Reader/Processor são obrigatórios, Processor opcional
+     * @return
+     */
+    public Step imprimeParImparStep(){
         return stepBuilderFactory
-                .get("imprimeOlaStep")
-                .tasklet(imprimeOlaTasklet(null))
+                .get("imprimeParImparStep")
+                .<Integer, String>chunk(1) //Ler Integer e escreve String, tamanho do chunck
+                .reader(contaAteDez())
+                .processor(parOuImparProcessor())
+                .writer(imprimeWriter())
             .build();
+    }
+
+    private ItemWriter<String> imprimeWriter() {
+        return itens -> itens.forEach(System.out::println);
+    }
+
+    private FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+        return new FunctionItemProcessor<Integer, String>
+                (item -> item % 2 == 0 ? String.format("Item %s é Par", item) : String.format("Item %s é Impar", item));
+    }
+
+    private IteratorItemReader<Integer> contaAteDez() {
+        return new IteratorItemReader<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10).iterator());
     }
 
     @Bean
